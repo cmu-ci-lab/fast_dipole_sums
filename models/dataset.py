2e8b763f610f5431a107757c433c554029490ad0
Hanyu Chen <hanyu.c.536@gmail.com>
2025-01-12 18:21:22 -0500

    Support images with varying dimensions

--- models/dataset.py
+++ models/dataset.py
@@ -51,9 +51,8 @@ class Dataset:
         self.camera_dict = camera_dict
         self.images_lis = sorted(glob(os.path.join(self.data_dir, 'image/*.png')) + glob(os.path.join(self.data_dir, 'image/*.jpg')))
         self.n_images = len(self.images_lis)
-        self.images_np = np.stack([cv.imread(im_name) for im_name in self.images_lis]) / 256.0
-        # self.masks_lis = sorted(glob(os.path.join(self.data_dir, 'mask/*.png')))
-        # self.masks_np = np.stack([cv.imread(im_name) for im_name in self.masks_lis]) / 256.0
+        # self.images_np = np.stack([cv.imread(im_name) for im_name in self.images_lis]) / 256.0
+        self.images_np = [cv.imread(im_name) / 256.0 for im_name in self.images_lis]
 
         # world_mat is a projection matrix from world to image
         self.world_mats_np = [camera_dict['world_mat_%d' % idx].astype(np.float32) for idx in range(self.n_images)]
@@ -73,14 +72,17 @@ class Dataset:
             self.intrinsics_all.append(torch.from_numpy(intrinsics).float())
             self.pose_all.append(torch.from_numpy(pose).float())
 
-        self.images = torch.from_numpy(self.images_np.astype(np.float32)).cpu()  # [n_images, H, W, 3]
-        # self.masks  = torch.from_numpy(self.masks_np.astype(np.float32)).cpu()   # [n_images, H, W, 3]
+        # self.images = torch.from_numpy(self.images_np.astype(np.float32)).cpu()  # [n_images, H, W, 3]
+        self.images = [torch.from_numpy(image.astype(np.float32)).cpu() for image in self.images_np]  # [n_images, H, W, 3]
         self.intrinsics_all = torch.stack(self.intrinsics_all).to(self.device)   # [n_images, 4, 4]
         self.intrinsics_all_inv = torch.inverse(self.intrinsics_all)  # [n_images, 4, 4]
         self.focal = self.intrinsics_all[0][0, 0]
         self.pose_all = torch.stack(self.pose_all).to(self.device)  # [n_images, 4, 4]
-        self.H, self.W = self.images.shape[1], self.images.shape[2]
-        self.image_pixels = self.H * self.W
+        # self.H, self.W = self.images.shape[1], self.images.shape[2]
+        self.H = [image.shape[0] for image in self.images]
+        self.W = [image.shape[1] for image in self.images]
+
+        print(len(self.intrinsics_all_inv))
 
         object_bbox_min = np.array([-1.01, -1.01, -1.01, 1.0])
         object_bbox_max = np.array([ 1.01,  1.01,  1.01, 1.0])
@@ -97,9 +99,12 @@ class Dataset:
         """
         Generate rays at world space from one camera.
         """
+        H = self.H[img_idx]
+        W = self.W[img_idx]
+
         l = resolution_level
-        tx = torch.linspace(0, self.W - 1, self.W // l)
-        ty = torch.linspace(0, self.H - 1, self.H // l)
+        tx = torch.linspace(0, W - 1, W // l)
+        ty = torch.linspace(0, H - 1, H // l)
         pixels_x, pixels_y = torch.meshgrid(tx, ty)
         p = torch.stack([pixels_x, pixels_y, torch.ones_like(pixels_y)], dim=-1) # W, H, 3
         p = torch.matmul(self.intrinsics_all_inv[img_idx, None, None, :3, :3], p[:, :, :, None]).squeeze()  # W, H, 3
@@ -112,8 +117,11 @@ class Dataset:
         """
         Generate random rays at world space from one camera.
         """
-        pixels_x = torch.randint(low=0, high=self.W, size=[batch_size])
-        pixels_y = torch.randint(low=0, high=self.H, size=[batch_size])
+        H = self.H[img_idx]
+        W = self.W[img_idx]
+
+        pixels_x = torch.randint(low=0, high=W, size=[batch_size])
+        pixels_y = torch.randint(low=0, high=H, size=[batch_size])
         color = self.images[img_idx][(pixels_y.cpu(), pixels_x.cpu())]    # batch_size, 3
         # mask = self.masks[img_idx][(pixels_y.cpu(), pixels_x.cpu())]      # batch_size, 3
         p = torch.stack([pixels_x, pixels_y, torch.ones_like(pixels_y)], dim=-1).float()  # batch_size, 3
@@ -122,39 +130,6 @@ class Dataset:
         rays_v = torch.matmul(self.pose_all[img_idx, None, :3, :3], rays_v[:, :, None]).squeeze()  # batch_size, 3
         rays_o = self.pose_all[img_idx, None, :3, 3].expand(rays_v.shape) # batch_size, 3
         return torch.cat([rays_o.cpu(), rays_v.cpu(), color], dim=-1).cuda()    # batch_size, 7
-
-    def gen_rays_between(self, idx_0, idx_1, ratio, resolution_level=1):
-        """
-        Interpolate pose between two cameras.
-        """
-        l = resolution_level
-        tx = torch.linspace(0, self.W - 1, self.W // l)
-        ty = torch.linspace(0, self.H - 1, self.H // l)
-        pixels_x, pixels_y = torch.meshgrid(tx, ty)
-        p = torch.stack([pixels_x, pixels_y, torch.ones_like(pixels_y)], dim=-1)  # W, H, 3
-        p = torch.matmul(self.intrinsics_all_inv[0, None, None, :3, :3], p[:, :, :, None]).squeeze()  # W, H, 3
-        rays_v = p / torch.linalg.norm(p, ord=2, dim=-1, keepdim=True)  # W, H, 3
-        trans = self.pose_all[idx_0, :3, 3] * (1.0 - ratio) + self.pose_all[idx_1, :3, 3] * ratio
-        pose_0 = self.pose_all[idx_0].detach().cpu().numpy()
-        pose_1 = self.pose_all[idx_1].detach().cpu().numpy()
-        pose_0 = np.linalg.inv(pose_0)
-        pose_1 = np.linalg.inv(pose_1)
-        rot_0 = pose_0[:3, :3]
-        rot_1 = pose_1[:3, :3]
-        rots = Rot.from_matrix(np.stack([rot_0, rot_1]))
-        key_times = [0, 1]
-        slerp = Slerp(key_times, rots)
-        rot = slerp(ratio)
-        pose = np.diag([1.0, 1.0, 1.0, 1.0])
-        pose = pose.astype(np.float32)
-        pose[:3, :3] = rot.as_matrix()
-        pose[:3, 3] = ((1.0 - ratio) * pose_0 + ratio * pose_1)[:3, 3]
-        pose = np.linalg.inv(pose)
-        rot = torch.from_numpy(pose[:3, :3]).cuda()
-        trans = torch.from_numpy(pose[:3, 3]).cuda()
-        rays_v = torch.matmul(rot[None, None, :3, :3], rays_v[:, :, :, None]).squeeze()  # W, H, 3
-        rays_o = trans[None, None, :3].expand(rays_v.shape)  # W, H, 3
-        return rays_o.transpose(0, 1), rays_v.transpose(0, 1)
 
     def near_far_from_sphere(self, rays_o, rays_d, bounding_sphere=1.0):
         a = torch.sum(rays_d**2, dim=-1, keepdim=True)
@@ -168,5 +143,6 @@ class Dataset:
 
     def image_at(self, idx, resolution_level):
         img = cv.imread(self.images_lis[idx])
-        return (cv.resize(img, (self.W // resolution_level, self.H // resolution_level))).clip(0, 255)
+        H, W = img.shape[:2]
+        return (cv.resize(img, (W // resolution_level, H // resolution_level))).clip(0, 255)
 
